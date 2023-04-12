5c7dd41d3064bf6486d379e743a83ce22f0bd905
Catalina Davis <100990168+catdavis25@users.noreply.github.com>
2023-04-12 01:02:33 -0700

    Add files via upload

---
+++ LetterGrade.asm
@@ -0,0 +1,6 @@
+#Names: Catalina Davis, Kenneth Hung, Ayanna Sanges-Chu
+#Date: 04/16/23
+#Objectives: Return a letter grade given Decimal int from user
+# - includse user menu, main label, looping label, exit label
+# - a way for user to continue getting letter grades or choose to exit
+# - invalid input handling
---
+++ advancedMath.asm
@@ -0,0 +1,6 @@
+#Names: Catalina Davis, Kenneth Hung, Ayanna Sanges-Chu
+#Date: 04/16/23
+#Objectives: takes two int from user that outputs the one of ints to the power of the other
+# - One int will be 'x', the other 'y'. The user must know which is which 
+# - Result = x^y and return to user
+# - Includes a main label, a looping label, exit label and loop counter
