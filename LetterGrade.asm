79b7d089bb080a44df6fc62f72734da77805d0aa
Cat <catdavis25@gmail.com>
2023-04-12 14:00:04 -0700

    menu strings

--- LetterGrade.asm
+++ LetterGrade.asm
@@ -4,3 +4,36 @@
 # - includse user menu, main label, looping label, exit label
 # - a way for user to continue getting letter grades or choose to exit
 # - invalid input handling
+
+.data 
+menu: .asciiz "~~~~~~~~~~~~~~Main Menu~~~~~~~~~~~~~~\n(1)Get Letter Grade \n(2)Exit program \n"
+menuChoice: .asciiz "\n Enter '1' or '2' for your selection: "
+instruction: .asciiz "\n Please enter a score as an integer value: "
+returnScore: .asciiz "\n The grade is: "
+newScore: .asciiz "\n Would you like to enter a new score? \n (Y)Yes  (N)No"
+newScoreSelection: .asciiz "Enter 'Y' or 'N' for your selection: "
+.text
+main:
+
+Menu:
+	#print out menu string
+	li $v0, 4
+	la $a0, menu
+	syscall
+	
+	#print out menu choice
+	li $v0, 4
+	la $a0, menuChoice
+	syscall
+	
+	li $v0, 5
+	syscall
+	move $t7, $v0
+	
+	
+
+
+
+exit:
+	li $v0, 10
+	syscall
